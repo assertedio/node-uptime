ef3de2c3cb9c9b70fe38a37735f7dbe162882462
Eric Hacke <eric@roleup.io>
2020-06-25 14:25:22 -0400

    initial version (#1)

---
+++ app.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.all('/echo', (req, res) => res.json({
+  method: req.method,
+  body: req.body || null,
+  query: Object.keys(req.query).length > 0 ? req.query : null,
+}));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
